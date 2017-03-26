50b10e58623cdef0d918f541f87d2cc52c64b621
Doug Franklin <doug@dougfranklin.com>
2017-03-26 07:06:10 -0400

    CamperMate: Tabs Are Visible

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+import { LocationPage }    from '../location/location';
+import { MyDetailsPage }   from '../my-details/my-details';
+import { CampDetailsPage } from '../camp-details/camp-details';
 
 import { NavController } from 'ionic-angular';
 
@@ -6,7 +9,12 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+
+  tab1Root: any = LocationPage;
+  tab2Root: any = MyDetailsPage;
+  tab3Root: any = CampDetailsPage;
 
   constructor(public navCtrl: NavController) {
     
