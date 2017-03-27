d5af697be38a50200aeb46dd70fba40b4ebd1381
Doug Franklin <doug@dougfranklin.com>
2017-03-27 13:51:39 -0400

    My Details HTML Updated

--- src/pages/camp-details/camp-details.ts
+++ src/pages/camp-details/camp-details.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Data } from '../../providers/data';
 
 /*
   Generated class for the CampDetails page.
@@ -11,10 +13,18 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-camp-details',
   templateUrl: 'camp-details.html'
 })
+
 export class CampDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, 
+    public dataService: Data, public navParams: NavParams) {
 
+      // 
+
+    }
+
+  saveForm(): void {}
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad CampDetailsPage');
   }
